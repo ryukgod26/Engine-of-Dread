7c3125dd366bf2deca80d07762551d42fdc950bf
raviwhitehat <ekravi26@gmail.com>
2025-11-02 21:00:54 +0530

    Almost Completed the first version

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,19 +3,23 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@onready var spot_light: SpotLight3D = $SpotLight3D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_pressed("light"):
+		spot_light.visible = !spot_light.visible
 	var input_dir := Input.get_vector("move_left","move_right","move_forward","move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
