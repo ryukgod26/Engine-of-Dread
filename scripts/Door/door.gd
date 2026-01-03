0332c4937bc3bfb558ac84df379f90f62ee0aec8
raviwhitehat <ekravi26@gmail.com>
2026-01-03 14:48:55 +0530

    feat: Add door model with toggle functionality and animations

---
+++ scripts/Door/door.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+var opened := false
+
+func  toggle_state():
+	opened = !opened
+	
--- scripts/Player/player_head.gd
+++ scripts/Player/player_head.gd
@@ -9,4 +9,4 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
 		rotate_x(deg_to_rad(-event.relative.y * sensitivity))
-		rotation.x = clamp(rotation.x,deg_to_rad(-90),deg_to_rad(90)) 
+		rotation.x = clamp(rotation.x,deg_to_rad(-90),deg_to_rad(90))
