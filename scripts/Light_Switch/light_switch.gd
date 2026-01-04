344162aeff5b2416fdf237b122751453adc0805e
raviwhitehat <ekravi26@gmail.com>
2026-01-04 15:39:49 +0530

    feat: Add light switch functionality and enhance light bulb interactions

--- scripts/Light_Switch/light_switch.gd
+++ scripts/Light_Switch/light_switch.gd
@@ -3,3 +3,24 @@ extends Node3D
 @export var on := false
 @export var on_mat: StandardMaterial3D
 @export var off_mat: StandardMaterial3D
+@export var light_bulb: Node3D
+
+func _ready() -> void:
+	if on:
+		light_bulb.get_node('light').material_override = on_mat
+	else:
+		light_bulb.get_node('light').material_override = off_mat
+	light_bulb.get_node('OmniLight').visible = on
+
+func toggle_light():
+	print("Toggle Function is Called")
+	on = not on
+	if on:
+		$on.visible = true
+		$off.visible = false
+		light_bulb.get_node('light').material_override = on_mat
+	else:
+		$on.visible = false
+		$off.visible = true
+		light_bulb.get_node('light').material_override = off_mat
+	light_bulb.get_node('OmniLight').visible = on
--- scripts/Light_Switch/light_switch_static_body.gd
+++ scripts/Light_Switch/light_switch_static_body.gd
@@ -1,11 +1,7 @@
-extends MeshInstance3D
+extends StaticBody3D
 
+@export var switch: Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func interact():
+	print("Interact Function is Called")
+	switch.toggle_light()
