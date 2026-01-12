5dee7bdb7e8e69e5c7807e2e8199552621debc12
raviwhitehat <ekravi26@gmail.com>
2026-01-12 16:20:23 +0530

    Improve drawer collision shape and update UI reticle visibility logic

--- scripts/Player/player_interact.gd
+++ scripts/Player/player_interact.gd
@@ -1,8 +1,14 @@
 extends RayCast3D
 
+@onready var ui: Control = $"../../UI"
+
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var collider = get_collider()
 		if collider.is_in_group("interectable") and collider.has_method('interact'):
+			ui.reticle_visible = true
 			if Input.is_action_just_pressed("interact"):
 				collider.interact()
+			
+	else:
+			ui.reticle_visible = false
--- scripts/UI/ui.gd
+++ scripts/UI/ui.gd
@@ -3,8 +3,14 @@ extends Control
 @export var dot_radius := 1.
 @export var dot_color := Color.WHITE
 
+var reticle_visible := false:
+	set(new_val):
+		reticle_visible = new_val
+		queue_redraw()
+
 func _ready() -> void:
 	queue_redraw()
 
 func _draw() -> void:
-	draw_circle($CanvasLayer/CenterContainer.position,dot_radius,dot_color,true)
+	if reticle_visible:
+		draw_circle($CanvasLayer/CenterContainer.position,dot_radius,dot_color,true)
