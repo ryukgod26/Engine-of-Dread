a1d310eb4d9ade3c4f46c6970a2e4ee6f31f1e58
raviwhitehat <ekravi26@gmail.com>
2026-01-21 01:32:04 +0530

    Add new fonts and update pause menu UI
    - Added Orbitron variable font and static font files for enhanced typography.
    - Included IM Fell English SC font files for additional stylistic options.
    - Updated pause menu scene to utilize new fonts and improved layout.
    - Enhanced button styles and animations in the pause menu for better user experience.
    - Integrated shader effects in the main menu for visual appeal.
    - Implemented animation transitions for smoother scene changes.

--- scripts/Main_Menu/main_menu.gd
+++ scripts/Main_Menu/main_menu.gd
@@ -4,6 +4,8 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_pressed() -> void:
+	$AnimationPlayer.play("Change")
+	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://scenes/loading_screens/game_loading_screen.tscn")
 
 func _on_options_pressed() -> void:
--- scripts/UI/pause_menu.gd
+++ scripts/UI/pause_menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause") and not $Options.visible:
 		paused = not paused
-		
+		$AnimationPlayer.play("Open")
 		if paused:
 			visible = true
 			get_tree().paused = true
